3aafac434078fc10fc0d731a08f5a68935e87f9e
yangshaobooo <1783494374@qq.com>
2023-07-20 10:09:08 +0800

    web

---
+++ routes/routes.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"goWebFrame/logger"
+	"net/http"
+)
+
+func Setup() *gin.Engine {
+	r := gin.New()
+	r.Use(logger.GinLogger(), logger.GinRecovery(true))
+	r.GET("/", func(context *gin.Context) {
+		context.String(http.StatusOK, "ok")
+	})
+	return r
+}
